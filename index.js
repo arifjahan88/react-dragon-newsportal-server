d0b599596de5d545a24332864b886398f358e4ab
Arif Jahan <arifjahan88@gmail.com>
2022-10-21 04:19:26 +0600

    after done authentication

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 app.use(cors());
 
 const categories = require("./Data/Categories.json");
+const news = require("./Data/News.json");
 
 app.get("/", (req, res) => {
   res.send("News Api is Running");
@@ -14,6 +15,24 @@ app.get("/", (req, res) => {
 app.get("/news-categories", (req, res) => {
   res.send(categories);
 });
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category = news.filter((n) => n.category_id === id);
+    res.send(category);
+  }
+});
+
 app.listen(port, () => {
   console.log("Dragon server is running", port);
 });
